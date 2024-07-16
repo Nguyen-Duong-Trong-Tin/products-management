8d1e70bc8516083a40bc4b38194a5d6fa16b6c30
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2024-07-16 21:36:18 +0700

    Config Views

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render("client/pages/home/index");
 });
 
 app.get('/products', (req, res) => {
-  res.send('Hello World!');
+  res.render("client/pages/products/index");
 });
 
 app.listen(port, () => {
