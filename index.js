fc6f01392a1d857ebe93c13d904d008f2bad41d1
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2024-07-18 16:29:34 +0700

    Config Admin Layout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const database = require("./config/database");
+const systemConfig = require("./config/system");
 
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
@@ -9,6 +10,9 @@ const app = express();
 const port = process.env.PORT;
 
 database.connect();
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
