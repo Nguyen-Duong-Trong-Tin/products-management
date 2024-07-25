6c281aca6b2998b08ad4c0fdaeb3d0d4b7a54ba3
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2024-07-25 16:16:55 +0700

    CONFIG VERCEL

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 database.connect();
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 
 // Routes
 routeAdmin(app);
